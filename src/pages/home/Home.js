17a8947a3f557650209dd0abe82aa196a7f5be61
KimSound424 <ykj1512@gmail.com>
2023-09-12 11:07:28 +0900

    [Feature] HomePage

---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import { Banner, DefaultHeader } from "../../components";
+
+const Home = () => {
+  return (
+    <>
+      <DefaultHeader />
+      <Banner />
+      <div>new arrivals</div>
+    </>
+  );
+};
+
+export default Home;
