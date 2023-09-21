ed6e9f355c7a4bdd7217280dfc2fc55d6d3dd90e
drawingcode1734 <lljy0213@naver.com>
2023-09-21 20:14:22 +0900

    [Style] productCard float 스타일 추가

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,13 +1,18 @@
+import StyledProductCard from "styles/components/StyledProductCard";
+
 // 상품 객체 하나를 받아와 카드를 만들어줌.
 
 const ProductCard = ({product}) => {
   return (
-    // 가장 바깥의 div태그에 onClick 붙여서 상세보기로 넘겨주자.
-    <div>
-      <img src="https://picsum.photos/200/300" alt="product" />
-      <p>{product.name}</p>
-      <p>{product.price.toLocaleString()}</p>
-    </div>
+      // 가장 바깥의 div태그에 onClick 붙여서 상세보기로 넘겨주자.
+
+    <StyledProductCard>
+      <div id="product">
+        <img src="https://picsum.photos/200/300" alt="product" />
+        <p>{product.name}</p>
+        <p>{product.price.toLocaleString()}</p>
+      </div>
+    </StyledProductCard>
   );
 };
 
---
+++ src/styles/components/StyledProductCard.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const StyledProductCard = styled.div`
+
+#product {
+    float: left;
+}
+`
+
+export default StyledProductCard;
