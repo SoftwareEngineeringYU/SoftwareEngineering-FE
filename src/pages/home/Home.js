61bb248d42a10c0757c421b7f4004743752dfe13
drawingcode1734 <lljy0213@naver.com>
2023-09-21 16:18:53 +0900

    [Style] default Header 스타일 추가

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,16 +1,20 @@
 import { DefaultHeader, PaginationBtn, ProductCard } from "../../components";
 import { Banner } from "./Banner";
 import sampleProductList from "../../assets/sampleProductList.json"
+import StyledHome from "styles/pages/home/StyledHome";
+
 
 const Home = () => {
   return (
-    <div>
-      <DefaultHeader />
-      <Banner />
-      <div>new arrivals</div>
-      <ProductCard product={sampleProductList[0]}/>
-      <PaginationBtn totalPages={6} />
-    </div>
+    <StyledHome>
+      <div>
+        <DefaultHeader />
+        <Banner />
+        <div>new arrivals</div>
+        <ProductCard product={sampleProductList[0]}/>
+        <PaginationBtn totalPages={6} />
+      </div>
+    </StyledHome>
   );
 };
 
