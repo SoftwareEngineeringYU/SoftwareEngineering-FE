b8869ce883958a5bf53dff926845fd7e3c5bca29
KimSound424 <ykj1512@gmail.com>
2023-11-08 15:29:48 +0900

    [Feature] 정적인 화면으로 변경

--- src/components/ReviewBox.js
+++ src/components/ReviewBox.js
@@ -1,32 +1,34 @@
-const ReviewBox = ({ review, loginUser }) => {
+const ReviewBox = () => {
   //별점 관련 변수
   const fullStar = 5;
   const filledStar = review.score;
   const emptyStar = fullStar - filledStar;
 
-
   return (
     <div>
-      <span>{review.title}</span>
-      <span>{review.writer}</span>
+      <span>리뷰 제목</span>
+      <span>작성자</span>
       {/* 별점을 보여주는 부분 */}
-      <span>
+      {/* <span>
         {[...Array(filledStar)].map((_, index) => (
           <span key={index}>★</span>
         ))}
-      </span>
-      <span>
+      </span> */}
+      {/* <span>
         {[...Array(emptyStar)].map((_, index) => (
           <span key={index}>☆</span>
         ))}
-      </span>
-      <div>{review.body}</div>
-      {review.writer === loginUser.name ? (
+      </span> */}
+      <span>별점</span>
+      <div>리뷰 내용</div>
+      {/* {review.writer === loginUser.name ? (
         <div>
           <span>수정</span> /
           <span> 삭제</span>
         </div>
-      ) : null}
+      ) : null} */}
+      <span>수정</span>
+      <span>삭제</span>
     </div>
   );
 };
