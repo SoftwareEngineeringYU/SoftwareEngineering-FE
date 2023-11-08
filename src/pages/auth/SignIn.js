03ccb8bd237122d140deddd928435d4252d20ccc
KimSound424 <ykj1512@gmail.com>
2023-11-08 13:51:27 +0900

    [Test] Network test

--- src/pages/auth/SignIn.js
+++ src/pages/auth/SignIn.js
@@ -1,11 +1,83 @@
 import StyledSignIn from "styles/pages/auth/StyledSignIn";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const SignIn = () => {
   const navigate = useNavigate();
 
   const navigateToSignUp = () => {
     navigate("/signUp");
+  };
+
+  const loginTest = async (e) => {
+    e.preventDefault();
+
+    await axios
+      .post(
+        "https://greencart.one/sapi/api/v1/auth/login",
+        {
+          email: "1@1.com",
+          password: "111111!q",
+        },
+        {
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const singUpTest = async (e) => {
+    e.preventDefault();
+
+    await axios
+      .post(
+        "https://greencart.one/sapi/api/v1/auth/signup",
+        {
+          nickname: "test",
+          email: "1@1.com",
+          password: "111111!q",
+        },
+        {
+          headers: {
+            withCredentials: true,
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "https://greencart.one/sapi",
+            Accept: "application/json",
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const getRequestTest = async (e) => {
+    e.preventDefault();
+
+    await axios
+      .get(
+        "https://greencart.one/sapi/api/v1/products?page=0&size=5&sort=name,desc",
+        {
+          headers: {
+            withCredentials: true,
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            Accept: "application/json",
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
@@ -27,6 +99,12 @@ const SignIn = () => {
           <button type="button" className="signUp" onClick={navigateToSignUp}>
             회원가입
           </button>
+          <button
+            onClick={getRequestTest}
+            style={{ width: "300px", height: "150px", marginTop: "20px" }}
+          >
+            Click To Test
+          </button>
         </div>
       </div>
     </StyledSignIn>
@@ -34,3 +112,18 @@ const SignIn = () => {
 };
 
 export default SignIn;
+
+// import axios from "axios";
+
+// const BASE_URL = process.env.REACT_APP_API;
+
+// const axiosInstance = axios.create({
+//   baseURL: BASE_URL,
+//   withCredentials: true
+// });
+
+// axiosInstance.defaults.headers.common["Content-Type"] = "application/json";
+// axiosInstance.defaults.headers.common["Access-Control-Allow-Origin"] = process.env.REACT_APP_API_DOMAIN;
+// axiosInstance.defaults.headers.common["Accept"] = "application/json";
+
+// export default axiosInstance;
